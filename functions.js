fa88609d9ac11e3e3712bfb781b5ec9c5188fe7d
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-06-23 04:09:51 +0300

    updates

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 function syncMinWidth() {
-  const col1 = document.querySelector('.column.left');
-  const col2 = document.querySelector('.column.right');
+  const col1 = document.querySelector('.column:first-child');
+  const col2 = document.querySelector('.column:last-child');
   if (col1 && col2) {
     const width = col1.getBoundingClientRect().width;
     col2.style.minWidth = width + 'px';
   }
 }
 
-// Run on load and on resize
+// Run on load and on window resize
 window.addEventListener('load', syncMinWidth);
 window.addEventListener('resize', syncMinWidth);
